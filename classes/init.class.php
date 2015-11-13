380f6345958bf530fc64d9ab5a8e5ab028d7c852
danleyb2 <ndieksman@gmail.com>
2015-11-13 22:34:15 +0300

    create initializer class called on every request

---
+++ classes/init.class.php
@@ -0,0 +1,39 @@
+<?php
+
+final class Init{
+    function __construct(){
+        global $session;
+        error_reporting(E_ALL|E_STRICT);
+        $this->define_configs();
+        spl_autoload_register(array($this,'classLoader'));
+        if(!isset($session)) {
+            $session = new Session();
+        }
+
+    }
+    function define_configs(){
+        defined('DS')?:define('DS',DIRECTORY_SEPARATOR);
+        defined('SITE_ROOT')?:define('SITE_ROOT',dirname(__DIR__));
+        defined('CONFIG')?:define('CONFIG',SITE_ROOT.DS.'config');
+        defined('PUBLIC')?:define('PUBLIC',SITE_ROOT.DS.'public');
+        defined('CLASSES')?:define('CLASSES',SITE_ROOT.DS.'classes');
+        defined('PAGES')?:define('PAGES',SITE_ROOT.DS.'pages');
+        defined('VIEWS')?:define('VIEWS',SITE_ROOT.DS.'views');
+        defined('CACHE')?:define('CACHE',SITE_ROOT.DS.'cache');
+        defined('TEMPLATES')?:define('TEMPLATES',SITE_ROOT.DS.'templates');
+    }
+    function classLoader($className){
+        $class_file=CLASSES.DS.strtolower($className).'.class.php';
+        if(!file_exists($class_file)){
+            exit('Class ['.$className.'] could not be loaded.<br> Looked in ['.$class_file.']');
+        }
+        /** @noinspection PhpIncludeInspection */
+        require_once $class_file;
+    }
+
+}
+$session;
+new Init();
+
+?>
+
