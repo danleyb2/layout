2b8de05e42028daa71b707f8e0dc2dda0420a2c3
danleyb2 <ndieksman@gmail.com>
2015-11-14 18:37:23 +0300

    create `404` page

---
+++ pages/404.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brian
+ * Date: 11/13/15
+ * Time: 11:45 AM
+ */
+?>
+
+<h1 style=" margin-top:20px;text-align:center;color:red;">404.</h1>
+<p class="" style="text-align:center;">The requested resource was not found.</p>
+<style>
+    pre{
+        padding:3px;
+        background-color: lemonchiffon;
+        border: 1px solid black;
+        border-radius: 4px;
+        width:50%;
+        margin:auto;
+    }
+
+</style>
+<?php
+Functions::print_prep(Functions::get_path());
+Functions::print_prep($_SERVER);
+?>
