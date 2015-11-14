15f9879faae23840cec4e6d6097e644ef67fe28e
danleyb2 <ndieksman@gmail.com>
2015-11-14 18:46:17 +0300

    database access configurations

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brian
+ * Date: 11/13/15
+ * Time: 7:55 PM
+ */
+
+defined('DBHOST')? :define('DBHOST','localhost');
+defined('DBUSER')? :define('DBUSER','root');
+defined('DBPASSWORD')? :define('DBPASSWORD','#####');
+defined('DB')? :define('DB','test');
+
+?>
