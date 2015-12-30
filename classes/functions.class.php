990c31eaf8f600cb01bade13eb4608b23891ea01
danleyb2 <ndieksman@gmail.com>
2015-12-30 19:15:36 +0300

    assert queries exist when getting path

--- classes/functions.class.php
+++ classes/functions.class.php
@@ -78,6 +78,7 @@ class Functions {
         if(isset($_SERVER['REQUEST_URI'])){
 
             $request_path=explode('?',$_SERVER['REQUEST_URI']);
+
             $path['base']=rtrim(dirname($_SERVER['SCRIPT_NAME']),'\/');
             $path['call_utf8']=substr(urldecode($request_path[0]),strlen($path['base'])+1);
             $path['call']=utf8_decode($path['call_utf8']);
@@ -85,6 +86,7 @@ class Functions {
                 $path['call']='';
             }
             $path['call_parts']=explode('/',rtrim($path['call'],'/'));
+            if(empty($request_path[1]))return $path;
             $path['query_utf8']=urldecode($request_path[1]);
             $path['query']=utf8_decode(urldecode($request_path[1]));
             $vars=explode('&',$path['query']);
@@ -93,6 +95,7 @@ class Functions {
                 $path['query_vars'][$t[0]]=$t[1];
             }
         }
+        //self::print_prep($path);exit;
         return $path;
     }
     public static function log($log){
