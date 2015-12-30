b8dc5c362c3c855d5244793f386914ef7b2e74c3
danleyb2 <ndieksman@gmail.com>
2015-12-30 18:47:51 +0300

    create a View class used to create and render views

---
+++ classes/view.class.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brian
+ * Date: 12/30/15
+ * Time: 3:29 PM
+ */
+
+class View{
+    public $header=true;
+    public $footer=true;
+    public $view_name;
+
+    function __construct($view_name=''){
+        $this->view_name=$view_name;
+    }
+
+    function has_header($t){
+        $this->header=$t;
+    }
+    function has_footer($t){
+        $this->footer=$t;
+    }
+
+    function create($view_name){
+        //define main container template name
+        #if not exist-> 404
+        $this->view_name=$view_name;
+        $view=$view_name;
+
+        if(!(file_exists(VIEWS.DS.$view.'.phtml')|file_exists(SCRIPTS.DS.$view.'.php'))){
+            disp404();
+            exit();
+        }
+
+        //run php script
+        /** @noinspection PhpIncludeInspection */
+        include_once SCRIPTS.DS.$view.'.php';
+
+        //include layout
+        //start caching
+        /** @noinspection PhpIncludeInspection */
+        #include_once TEMPLATES.DS.'layout.phtml';
+        //caching end
+    }
+    function render(){
+        global $session;
+        include_once TEMPLATES.DS.'layout.phtml';
+
+    }
+}
