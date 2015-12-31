0c205ca9166e5f367d325e1e21ba5cce31149da5
danleyb2 <ndieksman@gmail.com>
2015-12-31 10:11:24 +0300

    now is a class that implements controller class.. (i think this is better)

--- controllers/home.php
+++ controllers/home.php
@@ -6,11 +6,53 @@
  * Time: 3:07 PM
  */
 
+//print_r($request);
+use core\Controller;
 
-$view=new View();
-$view->create('main');
-$view->has_header(true);
-$view->has_footer(true);
-#$view->cache();
-$view->render();
+#require_once '/home/brian/PhpstormProjects/Layout/classes/controller.class.php';
+class Home extends Controller
+{
+    function get($req)
+    {
+        $view=new View();
+        $view->create('main');
+        $view->has_header(true);
+        $view->has_footer(true);
+        #$view->cache();
+        $view->render();
+    }
 
+    function post($req)
+    {
+        // TODO: Implement post() method.
+    }
+
+    function put($req)
+    {
+        // TODO: Implement put() method.
+    }
+
+    function patch($req)
+    {
+        // TODO: Implement patch() method.
+    }
+
+    function delete($req)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    function head($req)
+    {
+        // TODO: Implement head() method.
+    }
+
+    /**
+     * runs on all methods.
+     * @return mixed
+     */
+    function all($req)
+    {
+        // TODO: Implement all() method.
+    }
+}
