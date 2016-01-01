6a2bb011d60559299f3d875c6a447040af5ec0bf
danleyb2 <ndieksman@gmail.com>
2016-01-01 18:15:46 +0300

    create `Error` class - for formatting and displaying errors with custom views

---
+++ classes/error.class.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: brian
+ * Date: 1/1/16
+ * Time: 3:31 PM
+ */
+
+class Error {
+    private $message,$heading;
+    private $view_name='error_view';
+
+    function __construct($error_heading){
+        $this->heading=$error_heading;
+
+    }
+    function setHeading($heading){
+        $this->heading=$heading;
+    }
+    function setMessage($message){
+        $this->message=$message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHeading()
+    {
+        return $this->heading;
+    }
+
+    public function getViewName()
+    {
+        return $this->view_name;
+    }
+
+    /**
+     * @param mixed $view_name
+     */
+    public function setViewName($view_name='error_view')
+    {
+        $this->view_name = $view_name;
+    }
+
+}
