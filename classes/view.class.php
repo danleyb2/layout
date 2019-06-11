73c3bdc51c3ddcd2e7c7e3e71eef176ee1df1692
nyaundi brian <danleyb2@home.danleyb2.online>
2019-06-11 11:29:12 +0300

    wip

--- classes/view.class.php
+++ classes/view.class.php
@@ -6,13 +6,24 @@
  * Time: 3:29 PM
  */
 
+/**
+ * Class View
+ * @property Controller $controller
+ */
 class View{
     public $header=true;
     public $footer=true;
     public $view_name;
+    private $script_file;
+    private $main_is_file;
+    public $data;
+    public $controller;
 
-    function __construct($view_name=''){
-        $this->view_name=$view_name;
+    /**
+     * @param $controller Controller
+     */
+    function __construct($controller){
+        $this->controller=$controller;
     }
 
     function has_header($t){
@@ -20,6 +31,33 @@ class View{
     }
     function has_footer($t){
         $this->footer=$t;
+    }
+    function script($script_file){
+        $this->script_file=$script_file;
+    }
+    function main_template($template,$is_file=true){
+        $this->view_name=$template;
+        $this->main_is_file=$is_file;
+        return;
+    }
+    function get_main(){
+        //Functions::print_prep($this);exit;
+        //Functions::print_prep($this);exit;
+        if($this->main_is_file){
+            require_once VIEWS.DS.$this->view_name.'.phtml';
+        }else{
+            echo $this->view_name;
+        }
+
+    }
+    function setData($key,$value,$overwrite=true){
+        if(!$overwrite && array_key_exists($key,$this->data)){
+            return;
+        }
+        $this->data[$key]=$value;
+    }
+    function getData($key){
+        return $this->data[$key];
     }
 
     function create($view_name){
@@ -34,19 +72,41 @@ class View{
         #include_once TEMPLATES.DS.'layout.phtml';
         //caching end
     }
-    function render(){
-        global $session;
-        $view=$this->view_name;
+    function render($run_script){
+        //todo start response
 
-        if(!(file_exists(VIEWS.DS.$view.'.phtml')|file_exists(SCRIPTS.DS.$view.'.php'))){
-            disp404();
-            exit();
+        global $session;
+
+
+        if($this->main_is_file) {
+            $view_file_markup = VIEWS . DS . $this->view_name . '.phtml';
+            $view_file_script = SCRIPTS . DS . $this->view_name . '.php';
+
+            if (!(file_exists($view_file_markup) | file_exists($view_file_script))) {
+                //dispNoView();
+
+                $error=new Error("View File Missing Error.");
+                $error->setMessage(
+                    'View file :[ '.$view_file_markup.' ] missing.<br>
+                     View script:[ '.$view_file_script.' ] missing.<br>
+                     Please correct the <b>view</b> name in <b>'.get_class($this->controller).' </b>controller.
+                    '
+                );
+                $this->view_name = $error->getViewName();
+                $this->data['error']=$error;
+                //exit();
+            }
         }
 
-        //run php script
+        if($run_script) {
+            //run php script
+            /** @noinspection PhpIncludeInspection */
+            include_once SCRIPTS . DS . $this->script_file . '.php';
+        }
         /** @noinspection PhpIncludeInspection */
-        include_once SCRIPTS.DS.$view.'.php';
         include_once TEMPLATES.DS.'layout.phtml';
+
+        //todo end response
 
     }
 }
